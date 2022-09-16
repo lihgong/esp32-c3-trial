58cf6f1b3db35cc91d9929bd16acef84f9601a64
lihgong <lihgong.cm90@nctu.edu.tw>
2022-09-16 21:56:40 +0800

    Initial test code submit

--- src/main.c
+++ src/main.c
@@ -1,1 +1,13 @@
-void app_main() {}
+#include<stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+void app_main() 
+{
+    uint32_t i=0;
+    while(1) {
+        printf("Hello World %d\n", i++);
+        vTaskDelay(200/portTICK_RATE_MS);
+    }
+}
+
